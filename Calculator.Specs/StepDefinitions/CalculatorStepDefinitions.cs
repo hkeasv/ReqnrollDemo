32addbca419a57ca9509a2a9373744aa6deb787c
henrikkyhl <henrikkyhl@hotmail.com>
2024-10-04 13:45:27 +0200

    Initial commit

---
+++ Calculator.Specs/StepDefinitions/CalculatorStepDefinitions.cs
@@ -0,0 +1,37 @@
+using Reqnroll;
+using Xunit;
+
+namespace Calculator.Specs.StepDefinitions;
+
+[Binding]
+public sealed class CalculatorStepDefinitions
+{
+    // For additional details on Reqnroll step definitions see https://go.reqnroll.net/doc-stepdef
+
+    private readonly Calculator _calculator = new Calculator();
+    private int _result;
+
+    [Given("the first number is {int}")]
+    public void GivenTheFirstNumberIs(int number)
+    {
+        _calculator.FirstNumber = number;
+    }
+
+    [Given("the second number is {int}")]
+    public void GivenTheSecondNumberIs(int number)
+    {
+        _calculator.SecondNumber = number;
+    }
+
+    [When("the two numbers are added")]
+    public void WhenTheTwoNumbersAreAdded()
+    {
+        _result = _calculator.Add();
+    }
+
+    [Then("the result should be {int}")]
+    public void ThenTheResultShouldBe(int result)
+    {
+        Assert.Equal(result, _result);
+    }
+}
